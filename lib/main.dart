9b3bbc42dcab71a8c8fe6c6f30ed371bdf8e5fed
sankalp srivatava <Sankalp2015415@akgec.ac.in>
2022-08-22 23:54:48 +0530

    recent

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ Future<void> main() async {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
 // <<<<<<< HEAD
-    home: RootApp(),
+    home: AadharAuth(),
 // =======
 //     home: AadharAuth(),
 // >>>>>>> master
