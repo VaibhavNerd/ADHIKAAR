ed4d88068cdfdf5f30a04b840fe7732b0d570e88
vaibh <vaibhavdudeperfect@gmail.com>
2022-08-26 16:50:26 +0530

    multi-language2

--- lib/pages/raise.dart
+++ lib/pages/raise.dart
@@ -233,6 +233,7 @@ class _RaisePageState extends State<RaisePage> {
                           textAlign: TextAlign.center,
                         ),
                         SizedBox(height: 30.0),
+
                         Container(
                           decoration:
                               ThemeHelper().buttonBoxDecoration(context),
